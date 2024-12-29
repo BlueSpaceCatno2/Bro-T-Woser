03dbd661df110e4ab91ff62d987b41672a8a15c1
Brian <137111431+BlueSpaceCatno2@users.noreply.github.com>
2024-12-28 19:00:44 -0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,13 @@ namespace Project_T
 {
     public partial class Form1 : Form
     {
+        public Microsoft.Web.WebView2.Core.CoreWebView2Deferral Deferral;
+        public Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs Args;
+
         public Form1()
         {
             InitializeComponent();
         }
+        
     }
 }
